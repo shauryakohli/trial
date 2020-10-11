a32c8f144bb4ac83f19f1fd2043d29bed880e60c
Shaurya <shauryakohli17@gmail.com>
2020-10-11 16:45:34 +0530

    Update 5

--- age.py
+++ age.py
@@ -4,7 +4,7 @@ if n>18:
     print("You are a adult")
 elif n>60:
     print("You are a senior citizen")
-elif n>80:
+elif n>70:
     print("You are about to be dead")
 else:
     print("You are a kid")
