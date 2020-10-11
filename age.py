d0f92d31d37c07a99e41f320af2bd2e3bf892ee5
Shaurya <shauryakohli007@yahoo.com>
2020-10-11 14:10:44 +0530

    First version of age code

---
+++ age.py
@@ -0,0 +1,8 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python37/age.py ====
+What is your age
+28
+You are a adult
+>>> 
