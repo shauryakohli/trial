a0ab7ba1008eb5198b7ed4ddd76bf913fa3385f6
Shaurya <shauryakohli007@yahoo.com>
2020-10-11 15:58:52 +0530

    added age modification

--- age.py
+++ age.py
@@ -1,8 +1,8 @@
-Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-==== RESTART: C:/Users/Dell/AppData/Local/Programs/Python/Python37/age.py ====
-What is your age
-28
-You are a adult
->>> 
+print("What is your age")
+n=int(input())
+if n>18:
+    print("You are a adult")
+elif n>60:
+    print("You are a senior citizen")
+else:
+    print("You are a kid")
